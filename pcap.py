1d57f40bcdfd182761a46cbef5e0ccc9805b7246
Ryan Romanchuk <rromanchuk@gmail.com>
2017-08-25 13:40:10 -0700

    fix undefined fatal from bad merge

--- pcap.py
+++ pcap.py
@@ -115,12 +115,12 @@ class Cache(object):
                         _data = data.next()
                     except StopIteration:
                         break
-                except struct.error as err:
-                    logging.error("struct.error %s: %s", stream_id, err)
-                    try:
-                        _data = data.next()
-                    except StopIteration:
-                        break
+                # except struct.error as err:
+                #     logging.error("struct.error %s: %s", stream_id, err)
+                #     try:
+                #         _data = data.next()
+                #     except StopIteration:
+                #         break
                 except OverflowError as err:
                     logging.error("OverflowError %s: %s", stream_id, err)
                     try:
